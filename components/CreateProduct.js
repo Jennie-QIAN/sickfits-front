e0b8bfdc5e2fe8d99d83bbd93dac996885ce102d
Zhenni Qian <zhenni.qian913@gmail.com>
2021-04-14 23:24:19 +0200

    style add product form component

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,42 +1,53 @@
-import useForm from '../lib/useForm';
+import useForm from "../lib/useForm";
+import Form from "./styles/Form";
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
-    name: '',
+    image: "",
     price: 34234,
-    description: '',
+    description: "These are the best!",
   });
   return (
-    <form>
-      <label htmlFor="name">
-        Name
-        <input
-          type="test"
-          id="name"
-          name="name"
-          placeholder="name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="price"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-      <button type="button" onClick={resetForm}>
-        Rest Form
-      </button>
-    </form>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
