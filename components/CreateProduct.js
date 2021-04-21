2ac4f445f03acd87e30a66887f56ea19c5c3b275
Zhenni Qian <zhenni.qian913@gmail.com>
2021-04-21 23:39:14 +0200

    feat: create products via graphql mutation

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,20 +1,56 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import DisplayError from "./ErrorMessage";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
     image: "",
+    name: "",
     price: 34234,
     description: "These are the best!",
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
@@ -22,6 +58,16 @@ export default function CreateProduct() {
             type="file"
             id="image"
             name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            required
+            id="name"
+            name="name"
+            value={inputs.name}
             onChange={handleChange}
           />
         </label>
